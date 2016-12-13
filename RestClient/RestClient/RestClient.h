f9647bf1ada63e55f34f961da6093f35c8a50f8a
Osman <sapfear13@yandex.ru>
2016-12-13 11:11:47 +0300

    Init Commit

---
+++ RestClient/RestClient/RestClient.h
@@ -0,0 +1,19 @@
+//
+//  RestClient.h
+//  RestClient
+//
+//  Created by Robot on 13.12.16.
+//  Copyright © 2016 Robot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RestClient.
+FOUNDATION_EXPORT double RestClientVersionNumber;
+
+//! Project version string for RestClient.
+FOUNDATION_EXPORT const unsigned char RestClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestClient/PublicHeader.h>
+
+
